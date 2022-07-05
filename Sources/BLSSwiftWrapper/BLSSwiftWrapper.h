0843baa62d87778a65caada3330cd3f2a1b80aab
anquii <50277099+anquii@users.noreply.github.com>
2022-07-05 17:10:53 +0100

    Added source files

---
+++ Sources/BLSSwiftWrapper/BLSSwiftWrapper.h
@@ -0,0 +1,5 @@
+#import <BLSSwiftWrapper/bls384_256.h>
+#import <BLSSwiftWrapper/bls.h>
+#import <BLSSwiftWrapper/bn_c384_256.h>
+#import <BLSSwiftWrapper/bn.h>
+#import <BLSSwiftWrapper/curve_type.h>
---
+++ Tests/BLSSwiftWrapperTests/BLSSwiftWrapperTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+import BLSSwiftWrapper
+
+final class BLSSwiftWrapperTests: XCTestCase {
+    func testGivenImportWrapper_WhenTestIntegration_ThenSuccess() {
+        fatalError()
+    }
+}
