3fa0f10eaaf2346abdabc979b13f549a72c046b6
anquii <50277099+anquii@users.noreply.github.com>
2022-07-06 10:12:25 +0100

    Updated BLSSwiftWrapper.h

--- Sources/BLSSwiftWrapper/BLSSwiftWrapper.h
+++ Sources/BLSSwiftWrapper/BLSSwiftWrapper.h
@@ -1,5 +1,8 @@
-#import <BLSSwiftWrapper/bls384_256.h>
+#define MCL_SIZEOF_UNIT 8
+#define MCL_MAX_FP_BIT_SIZE 384
+#define MCL_MAX_FR_BIT_SIZE 256
+#define MCLBN_FP_UNIT_SIZE 6
+#define MCLBN_FR_UNIT_SIZE 4
+#define BLS_ETH 1
+
 #import <BLSSwiftWrapper/bls.h>
-#import <BLSSwiftWrapper/bn_c384_256.h>
-#import <BLSSwiftWrapper/bn.h>
-#import <BLSSwiftWrapper/curve_type.h>
